73bd633d1cc432beaea03066e796e071ac061ebf
Jhio0 <120062814+Jhio0@users.noreply.github.com>
2024-01-04 16:01:12 -0700

    Added parallax

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import Head from "next/head";
 import Navbar from "./components/Navbar";
@@ -7,6 +8,8 @@ import Skills from "./components/Skills";
 import Projetcs from "./components/Projetcs";
 import Contact from "./components/Contact";
 
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
 const Page = () => {
   return (
     <div>
@@ -15,12 +18,32 @@ const Page = () => {
         <meta name="description" content="Generated bt create next app"></meta>
         <link rel="icon" href="/favicon.ico"></link>
       </Head>
-      <Navbar />
-      <Main />
-      <About />
-      <Skills/>
-      <Projetcs/>
-      <Contact/>
+
+      <Parallax pages={10}>
+        <Navbar />
+
+        <ParallaxLayer offset={0}>
+          <Main />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1}>
+          <About />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2}>
+          <Skills/>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={3}>
+          <Projetcs/>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={5}>     
+          <Contact/>
+        </ParallaxLayer>
+
+      </Parallax>
+      
     </div>
     
   );
