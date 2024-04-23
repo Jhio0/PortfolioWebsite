233740532266b9c9757119ecc3a0fd2ef008949f
Jhio0 <120062814+Jhio0@users.noreply.github.com>
2024-04-23 14:54:01 -0600

    New Changes

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,19 +4,18 @@ import {AiOutlineMail} from 'react-icons/ai'
 import {FaLinkedinIn, FaGithub} from 'react-icons/fa'
 import { motion} from 'framer-motion'
 
-
 const Main = () => {
     const text = "Coding visionary ready to bring your ideas to life.".split(" ");
     const name = "Hi, I am Jhio".split(" ");
     const jobDes = "A Software Developer".split("");
 
     return (
-        <div id='home' className='w-full h-screen'>
-            <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-start items-center'>
+        <div id='home' className='w-full h-screen flex justify-center items-center'>
+            <div className='max-w-[1240px] w-full mx-auto p-2 flex flex-col justify-center items-center'>
                 <div>
-                    <h1 className='py-4 text-gray-700'>
+                    <h1 className='py-4 text-gray-700 text-3xl md:text-4xl lg:text-5xl'>
                         {name.map((el, i) => (
-                                <motion.span
+                            <motion.span
                                 initial={{ opacity: 0 }}
                                 animate={{ opacity: 1 }}
                                 transition={{
@@ -24,14 +23,14 @@ const Main = () => {
                                     delay: i / 10,
                                 }}
                                 key={i}
-                                >
+                            >
                                 {el}{" "}
-                                </motion.span>
-                            ))}
+                            </motion.span>
+                        ))}
                     </h1>
-                    <h1 className='py-4 text-gray-700'>
+                    <h1 className='py-4 text-gray-700 text-3xl md:text-4xl lg:text-5xl'>
                         {jobDes.map((el, i) => (
-                                <motion.span
+                            <motion.span
                                 initial={{ opacity: 0 }}
                                 animate={{ opacity: 1 }}
                                 transition={{
@@ -39,23 +38,23 @@ const Main = () => {
                                     delay: i / 10,
                                 }}
                                 key={i}
-                                >
+                            >
                                 {el}{" "}
-                                </motion.span>
-                            ))}
+                            </motion.span>
+                        ))}
                     </h1>
-                    <div className="uppercase test-sm tracking-widest text-gray-600">
+                    <div className="uppercase text-sm tracking-widest text-gray-600 text-center">
                         {text.map((el, i) => (
                             <motion.span
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            transition={{
-                                duration: 0.30,
-                                delay: i / 10,
-                            }}
-                            key={i}
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{
+                                    duration: 0.30,
+                                    delay: i / 10,
+                                }}
+                                key={i}
                             >
-                            {el}{" "}
+                                {el}{" "}
                             </motion.span>
                         ))}
                     </div>
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
         <div className={shadow ? 'fixed w-full h-20 shadows-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <Link href='/'>
-                <button class="relative -top-1 -left-1 bg-gray-800 py-2.5 px-5 font-medium uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-gray-700 before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0">My Resume</button>
+                <button className="relative -top-1 -left-1 bg-gray-800 py-2.5 px-5 font-medium uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-gray-700 before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0">My Resume</button>
                 </Link>
                 <div>
                     <ul className='hidden md:flex'>
